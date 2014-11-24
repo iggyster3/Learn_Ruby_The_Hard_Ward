35e2392665961ba27f2fbce3b9959890f6d82816
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-11-24 20:26:41 +0000

    Modified ex7.rb

--- ex7.rb
+++ ex7.rb
@@ -1,6 +1,7 @@
 puts "Mary had a little lamb."
 puts "It's fleece was white as #{'snow'}."
 puts "And everywhere that Mary went."
+puts "." * 10
 
 end1 = "C"
 end2 = "h"
@@ -17,4 +18,4 @@ end12 = "r"
 
 # watch that print vs. puts on this line what's it do?
 print end1 + end2 + end3 + end4 + end5 + end6
-puts end7 + end8 + end9 + end10 + end11 + end12 
+puts end7 + end8 + end9 + end10 + end11 + end12
