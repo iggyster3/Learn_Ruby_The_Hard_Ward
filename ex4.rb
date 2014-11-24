2b30ec10facbe964d41384c5c9b2394107d08735
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-11-24 13:53:21 +0000

    Updated ex4.rb

--- ex4.rb
+++ ex4.rb
@@ -1,10 +1,25 @@
+#Variable Assignment for amount of cars
 cars = 100
-space_in_a_car = 4.0
+
+#Variable Assignment for space in a car
+space_in_a_car = 4
+
+#Variable Assignment for drivers
 drivers = 30
+
+#Variable Assignment for passengers
 passengers = 90
+
+#Variable calaculation for cars not driven
 cars_not_driven = cars - drivers
+
+#Variable Assignment for cars driven
 cars_driven = drivers
+
+#Variable calculation carpool
 carpool_capacity = cars_driven * space_in_a_car
+
+#Variable calculation for average passenger per car available in carpool
 average_passengers_per_car = passengers / cars_driven
 
 puts "There are #{cars} cars available."
