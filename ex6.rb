38fcc870cb394094b796a6f05f9fbeb7012d50d6
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-11-24 19:50:02 +0000

    Updated ex6.rb

--- ex6.rb
+++ ex6.rb
@@ -4,7 +4,7 @@ x = "There are #{types_of_people} types of people"
 binary  = "binary"
 do_not = "don't"
 
-y = "Those who #{binary}  and those that #{do_not}."
+y = "Those who know #{binary} and those that #{do_not}."
 
 puts x
 puts y
