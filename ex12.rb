51ca5d67253d48179067a698564bd47631ace673
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-11-25 13:31:36 +0000

    Updated ex12.rb

--- ex12.rb
+++ ex12.rb
@@ -1,12 +1,22 @@
-print "Give me a number:"
-number = gets.chomp.to_f
+print "Give me some money:"
+money = gets.chomp.to_f
 
-bigger = number * 100
-puts "A bigger number is #{bigger}"
+# 10% percentage
+percentage = 10 / 100.to_f
+calculation = percentage * money
+puts "Ten percent of #{money} is #{calculation}"
 
+=begin
 print "Give me another number: "
 another = gets.chomp
 number = another.to_f
 
 smaller = number / 100
 puts "A smaller number is #{smaller}"
+
+
+Discount	= 30% of 75
+= 30/100 × 75
+= 0.3 × 75
+= £22.50
+=end
