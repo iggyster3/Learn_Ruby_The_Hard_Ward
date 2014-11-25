3745eb182a219fd9d7849686b93bcc7af58bab8b
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-11-25 11:32:17 +0000

    Updated ex11.rb

--- ex11.rb
+++ ex11.rb
@@ -10,7 +10,12 @@ weight = gets.chomp
 puts "So you're #{age} old, #{height} tall and #{weight} in weight."
 
 print "Good Morning Mr.Ighodaro how much money do you have to deposit? "
-deposit_money = gets.chomp
+deposit_money = gets.chomp.to_i
 
-puts "Mr Ighodaro you have deposited #{deposit_money} into your current account.."
+total_money = deposit_money + 20
+
+puts "Mr Ighodaro you have deposited #{deposit_money}
+into your current account will we be adding #{"20"}
+so your total balance is #{total_money.to_s}"
+
 print "Goodbye.."
