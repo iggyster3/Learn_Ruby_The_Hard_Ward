2c4276e460996fe982d0746e9b1a14691f9c756c
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-11-25 10:10:27 +0000

    Updated ex9.rb

---
+++ ex9.rb
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tues Weds Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nJun\nJul\nAug\nSept\nOct\nNov\nDec"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There is something going on here
+with three double-quotes.
+We'll be able to type  as much as we like.
+Even 4 lines if we want want, or 5, or 6.
+""" 
