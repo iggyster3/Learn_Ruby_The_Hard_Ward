7182026595ebfe0a82f506b0001e53b20d07bfe0
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-11-25 18:10:17 +0000

    updated ex15.rb and ex16.rb

--- ex15.rb
+++ ex15.rb
@@ -7,13 +7,16 @@ txt = open(filename)
 # Print to stdout the filename given at the commandline and print the contents
 puts "Here's your file #{filename}"
 print txt.read
+txt.close()
+
 
 # Print to stdout and get input from user's keyboard
 print "Type the filename again: "
-file_again = $stdin.gets.chomp
+text_input = $stdin.gets.chomp
 
 # open and read contents of file
-txt_again = open(file_again)
+text_again = open(text_input)
 
 # Print contents of file to stdout(console)
-print txt_again.read
+print text_again.read
+text_again.close()
