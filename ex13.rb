b23923d60b365839fb308482453c6ba072728118
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-11-25 14:01:41 +0000

    Modified ex13.rb

---
+++ ex13.rb
@@ -0,0 +1,12 @@
+first, second, third, fourth, fifth = ARGV
+
+puts "Your first variable is: #{first}"
+
+extra_input = STDIN.gets.chomp()
+
+puts "#{extra_input}"
+
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+puts "Your fouth variable is: #{fourth}"
+puts "Your fifth variable is: #{fifth}"
